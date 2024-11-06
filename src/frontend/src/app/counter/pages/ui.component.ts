593bb7f8d4de1a153bc9db3a4795c32b8a6775ff
Thomas <thomasblatnik@gmail.com>
2024-11-06 10:52:14 -0500

    Counter lab step 3

--- src/frontend/src/app/counter/pages/ui.component.ts
+++ src/frontend/src/app/counter/pages/ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-ui',
@@ -7,11 +7,23 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   imports: [],
   template: `
     <div data-testid="counter-feature-ui">
-      <button class="btn btn-primary">-</button>
-      <span data-testid="current">0</span>
-      <button class="btn btn-primary">+</button>
+      <button [disabled]="decrementDisabled()"
+      (click)="decrement()" class="btn btn-primary">-</button>
+      <span data-testid="current">{{ count() }}</span>
+      <button (click)="increment()" class="btn btn-primary">+</button>
     </div>
   `,
   styles: ``,
 })
-export class UiComponent {}
+export class UiComponent {
+    count = signal(0);
+    decrementDisabled = computed(() => this.count() <= 0)
+
+    increment(){
+        this.count.update((c) => c + 1)
+    }
+
+    decrement(){
+        this.count.update((c) => c - 1)
+    }
+}
